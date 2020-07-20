921adfdbc9cd64a848ab2e9078b416d5643aae03
Alexander Wels <awels@redhat.com>
2020-07-20 11:53:16 -0400

    Update sig storage lib external provisioner to v6 Add test to see events being generated by operator on install and changing a managed object like the daemonset
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- vendor/k8s.io/apimachinery/pkg/apis/meta/v1/types.go
+++ vendor/k8s.io/apimachinery/pkg/apis/meta/v1/types.go
@@ -455,6 +455,7 @@ const (
 	DryRunAll = "All"
 )
 
+// +k8s:conversion-gen:explicit-from=net/url.Values
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 // DeleteOptions may be provided when deleting an API object.
@@ -470,6 +471,7 @@ type DeleteOptions struct {
 
 	// Must be fulfilled before a deletion is carried out. If not possible, a 409 Conflict status will be
 	// returned.
+	// +k8s:conversion-gen=false
 	// +optional
 	Preconditions *Preconditions `json:"preconditions,omitempty" protobuf:"bytes,2,opt,name=preconditions"`
 
@@ -871,6 +873,9 @@ const (
 	// FieldManagerConflict is used to report when another client claims to manage this field,
 	// It should only be returned for a request using server-side apply.
 	CauseTypeFieldManagerConflict CauseType = "FieldManagerConflict"
+	// CauseTypeResourceVersionTooLarge is used to report that the requested resource version
+	// is newer than the data observed by the API server, so the request cannot be served.
+	CauseTypeResourceVersionTooLarge CauseType = "ResourceVersionTooLarge"
 )
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
