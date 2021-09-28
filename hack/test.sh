11a9d9274557fae198a9a6115461d93e9dac5fc3
Alexander Wels <awels@redhat.com>
2021-09-28 09:35:23 -0500

    Update build to use PROW golang container in ci
    Use the golang container available in PROW system to run
    builds and unit tests. Added environment variable so we
    can pick which version of go we want to use.
    
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- hack/test.sh
+++ hack/test.sh
@@ -19,11 +19,12 @@ export KUBEVIRT_NUM_NODES=2
 export KUBEVIRT_PROVIDER=k8s-1.21
 make cluster-down
 make cluster-up
-wget https://dl.google.com/go/go1.16.7.linux-amd64.tar.gz
-tar -xzf go1.16.7.linux-amd64.tar.gz
-export GOROOT=$PWD/go
-export PATH=$GOROOT/bin:$PATH
-echo $PATH
+if [[ -v PROW_JOB_ID ]] ; then
+  GOLANG_VER=${GOLANG_VER:-1.16.8}
+  eval $(gimme ${GOLANG_VER})
+  cp -R ~/.gimme/versions/go${GOLANG_VER}.linux.amd64 /usr/local/go
+fi
+
 go get gotest.tools/gotestsum
 export UPGRADE_FROM=$(curl -s https://github.com/kubevirt/hostpath-provisioner-operator/releases/latest | grep -o "v[0-9]\.[0-9]*\.[0-9]*")
 echo "Upgrading from verions: $UPGRADE_FROM"
