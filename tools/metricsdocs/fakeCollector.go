f1b3c1718061fc58459b68ee3b019c418683dbe4
João Vilaça <jvilaca@redhat.com>
2021-12-02 15:44:43 +0000

    Add tool to generate metrics docs
    Signed-off-by: João Vilaça <jvilaca@redhat.com>

---
+++ tools/metricsdocs/fakeCollector.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"kubevirt.io/hostpath-provisioner/controller/metrics"
+)
+
+type fakeCollector struct {
+}
+
+func (fc fakeCollector) Describe(_ chan<- *prometheus.Desc) {
+}
+
+//Collect needs to report all metrics to see it in docs
+func (fc fakeCollector) Collect(ch chan<- prometheus.Metric) {
+	ps := metrics.NewPrometheusScraper(ch)
+	ps.Report("test")
+}
+
+func RegisterFakeCollector() {
+	prometheus.MustRegister(fakeCollector{})
+}
