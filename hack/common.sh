0adac6dcb8ce186332928437c406c177196094c0
Alexander Wels <awels@redhat.com>
2023-06-15 10:47:24 -0500

    Update golang version to 1.20.5
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- hack/common.sh
+++ hack/common.sh
@@ -12,7 +12,7 @@
 #See the License for the specific language governing permissions and
 #limitations under the License.
 
-GOLANG_VER=${GOLANG_VER:-1.19.4}
+GOLANG_VER=${GOLANG_VER:-1.20.5}
 
 function setGoInProw() {
   if [[ -v PROW_JOB_ID ]] ; then
