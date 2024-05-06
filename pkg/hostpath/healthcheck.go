840c1a8a65c0b2c099a2596b0ee4896b6f6bed60
Shelly Kagan <skagan@redhat.com>
2024-05-06 16:02:38 +0300

    Remove dependency of k8s.io/kubernetes
    As mentioned in k8s github repo:
    `Use of the k8s.io/kubernetes module or k8s.io/kubernetes/... packages
    as libraries is not supported.`
    Also removing this will resolve possible CVEs
    
    Signed-off-by: Shelly Kagan <skagan@redhat.com>

--- pkg/hostpath/healthcheck.go
+++ pkg/hostpath/healthcheck.go
@@ -20,8 +20,9 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"golang.org/x/sys/unix"
+
 	"k8s.io/klog/v2"
-	"k8s.io/kubernetes/pkg/volume/util/fs"
 )
 
 type MountPointInfo struct {
@@ -71,8 +72,33 @@ func checkMountPointExist(volumePath string) (bool, error) {
 	return false, nil
 }
 
+// fsInfo linux returns (available bytes, byte capacity, byte usage, total inodes, inodes free, inode usage, error)
+// for the filesystem that path resides upon.
+func fsInfo(path string) (int64, int64, int64, int64, int64, int64, error) {
+	statfs := &unix.Statfs_t{}
+	err := unix.Statfs(path, statfs)
+	if err != nil {
+		return 0, 0, 0, 0, 0, 0, err
+	}
+
+	// Available is blocks available * fragment size
+	available := int64(statfs.Bavail) * int64(statfs.Bsize)
+
+	// Capacity is total block count * fragment size
+	capacity := int64(statfs.Blocks) * int64(statfs.Bsize)
+
+	// Usage is block being used * fragment size (aka block size).
+	usage := (int64(statfs.Blocks) - int64(statfs.Bfree)) * int64(statfs.Bsize)
+
+	inodes := int64(statfs.Files)
+	inodesFree := int64(statfs.Ffree)
+	inodesUsed := inodes - inodesFree
+
+	return available, capacity, usage, inodes, inodesFree, inodesUsed, nil
+}
+
 func getPVStats(volumePath string) (available int64, capacity int64, used int64, inodes int64, inodesFree int64, inodesUsed int64, err error) {
-	return fs.FsInfo(volumePath)
+	return fsInfo(volumePath)
 }
 
 func checkPVUsage(volumePath string) (int64, int64, error) {
