dc7396a4fa47847188af42c92ac39966376ed217
Alexander Wels <awels@redhat.com>
2026-03-26 09:10:40 -0500

    Deploy new prometheus operator CRDs. so that the functional tests pass because the operator is trying to use the new CRD values.
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- hack/test.sh
+++ hack/test.sh
@@ -20,6 +20,20 @@ export KUBEVIRT_DEPLOY_PROMETHEUS=true
 make cluster-down
 make cluster-up
 
+# Apply updated Prometheus Operator CRDs matching the version in operator's go.mod
+OPERATOR_BRANCH="${OPERATOR_BRANCH:-main}"
+echo "Fetching Prometheus Operator version from hostpath-provisioner-operator/${OPERATOR_BRANCH}..."
+OPERATOR_GO_MOD_URL="https://raw.githubusercontent.com/kubevirt/hostpath-provisioner-operator/${OPERATOR_BRANCH}/go.mod"
+PROMETHEUS_OPERATOR_VERSION=$(curl -sL "${OPERATOR_GO_MOD_URL}" | grep "prometheus-operator/prometheus-operator/pkg/apis/monitoring" | awk '{print $2}')
+
+if [ -n "${PROMETHEUS_OPERATOR_VERSION}" ]; then
+    echo "Applying Prometheus Operator CRDs from ${PROMETHEUS_OPERATOR_VERSION}..."
+    ./cluster-up/kubectl.sh apply -f "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${PROMETHEUS_OPERATOR_VERSION}/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml"
+    ./cluster-up/kubectl.sh apply -f "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${PROMETHEUS_OPERATOR_VERSION}/example/prometheus-operator-crd/monitoring.coreos.com_prometheusrules.yaml"
+else
+    echo "Warning: Could not determine Prometheus Operator version from ${OPERATOR_GO_MOD_URL}"
+fi
+
 go install gotest.tools/gotestsum@latest
 #export UPGRADE_FROM=$(curl -s https://github.com/kubevirt/hostpath-provisioner-operator/releases/latest | grep -o "v[0-9]\.[0-9]*\.[0-9]*")
 #The upgrade from code path doesn't setup the webhook and cert-manager, only the code after the upgrade does. This is a critical test so we want
